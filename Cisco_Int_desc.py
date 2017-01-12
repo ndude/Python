278983213d7a13beb0716e6c4629e3e2dae85f53
ndude <erustamov@gmail.com>
2017-01-12 14:13:52 +0000

    Add files via upload

---
+++ Cisco_Int_desc.py
@@ -0,0 +1,25 @@
+import paramiko
+import time
+
+ip = raw_input("IP address: ")
+username = "cisco"
+password = "cisco"
+
+remote_conn_pre=paramiko.SSHClient()
+remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+remote_conn_pre.connect(ip, port=22, username=username,
+                        password=password,
+                        look_for_keys=False, allow_agent=False)
+
+remote_conn = remote_conn_pre.invoke_shell()
+output = remote_conn.recv(65535)
+print output
+
+cmds = ["conf t", "interface Ethernet 1/3", "descr \"Test number 1 python\"",
+ "exit", "exit", "wr mem"]
+
+for cmd in cmds:
+    remote_conn.send(cmd+"\n")
+    time.sleep(.5)
+    output = remote_conn.recv(65535)
+    print output
